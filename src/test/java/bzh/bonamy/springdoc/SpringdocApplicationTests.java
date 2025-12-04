da9e4809661be170f17229a8febeac458ace09b0
nicolasb29 <nicolas.bonamy2@gmail.com>
2025-12-04 13:20:20 +0000

    Upload

---
+++ src/test/java/bzh/bonamy/springdoc/SpringdocApplicationTests.java
@@ -0,0 +1,13 @@
+package bzh.bonamy.springdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
